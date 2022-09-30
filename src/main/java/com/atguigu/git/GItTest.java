0538fc07264fc16f84847aa3eca63e00dd7ae366
fly <yao_fei_yang@163.com>
2022-09-30 21:41:50 +0800

    master commit3

--- src/main/java/com/atguigu/git/GItTest.java
+++ src/main/java/com/atguigu/git/GItTest.java
@@ -6,12 +6,5 @@ package com.atguigu.git;
 public class GItTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
-        System.out.println("test");
-        System.out.println("test2");
-        System.out.println("666");
-        System.out.println("fly");
-        System.out.println("hello hot-fix");
-        System.out.println("master test");
-        System.out.println("hot-fix test");
     }
 }
